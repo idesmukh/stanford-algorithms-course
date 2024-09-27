d9d3b6bc844ab17a6858b64b0010d0ba605b5b93
idesmukh <ibaddesmukh@gmail.com>
2024-09-27 15:21:03 +0100

    Convert indendation to spaces

--- karatsuba.py
+++ karatsuba.py
@@ -12,14 +12,14 @@ Copyright (c) 2024 Ibad Desmukh. All rights reserved
 import math
 
 def count_digits(number):
-	"""Return the number of digits in the given number"""
-	if number == 0:
-		return 1
-	# log10 provides the power to raise 10 to in order to get the number, and floor rounds it to lowest integer
-	return math.floor(math.log10(abs(number))) + 1
+    """Return the number of digits in the given number"""
+    if number == 0:
+        return 1
+    # log10 provides the power to raise 10 to in order to get the number, and floor rounds it to lowest integer
+    return math.floor(math.log10(abs(number))) + 1
 
 def karatsuba(x, y):
-	"""
+    """
     Recursively multiply two positive integers using the Karatsuba algorithm
     
     Args:
@@ -28,45 +28,45 @@ def karatsuba(x, y):
     Returns:
     The product of x and y
     """
-	
-	# Base case: check if the input numbers are single-digit and if so return their product directly
-	if x < 10 and y < 10:
-		return x * y
-	
-	# Recursive case
-	# Calculate n, the number of digits in x and y
-	n_x, n_y = count_digits(x), count_digits(y)
+    
+    # Base case: check if the input numbers are single-digit and if so return their product directly
+    if x < 10 and y < 10:
+        return x * y
+    
+    # Recursive case
+    # Calculate n, the number of digits in x and y
+    n_x, n_y = count_digits(x), count_digits(y)
 
-	# Determine n, as maximum of n_x and n_y
-	n = max(n_x, n_y)
+    # Determine n, as maximum of n_x and n_y
+    n = max(n_x, n_y)
 
-	# Split x into a and b, y into c and d
-	a = x // (10**(n_x//2))
-	b = x % (10**(n_x//2))
-	c = y // (10**(n_y//2))
-	d = y % (10**(n_y//2))
+    # Split x into a and b, y into c and d
+    a = x // (10**(n_x//2))
+    b = x % (10**(n_x//2))
+    c = y // (10**(n_y//2))
+    d = y % (10**(n_y//2))
 
-	# Calculate p := a + b and q := c + d
-	p = a + b
-	q = c + d
+    # Calculate p := a + b and q := c + d
+    p = a + b
+    q = c + d
 
-	# Implement recursive multiplication
-	ac = karatsuba(a, c)
-	bd = karatsuba(b, d)
+    # Implement recursive multiplication
+    ac = karatsuba(a, c)
+    bd = karatsuba(b, d)
 
-	# Recursively compute (a + b).(c + d), instead of (a.d + b.c)
-	pq = karatsuba(p, q)
+    # Recursively compute (a + b).(c + d), instead of (a.d + b.c)
+    pq = karatsuba(p, q)
 
-	# Calculate adbc := pq - ac - bd which is equal to (a.d + b.c)
-	adbc = pq - ac - bd
+    # Calculate adbc := pq - ac - bd which is equal to (a.d + b.c)
+    adbc = pq - ac - bd
 
-	# Calculate each term of the formula
-	term_1 = 10**n * ac
-	term_2 = 10**(n // 2) * (adbc)
-	term_3 = bd
+    # Calculate each term of the formula
+    term_1 = 10**n * ac
+    term_2 = 10**(n // 2) * (adbc)
+    term_3 = bd
 
-	# Perform basic addition to compute x.y
-	return term_1 + term_2 + term_3
+    # Perform basic addition to compute x.y
+    return term_1 + term_2 + term_3
 
 # Test the function
 print(karatsuba(24, 36))
--- rec-int-mult.py
+++ rec-int-mult.py
@@ -12,14 +12,14 @@ Copyright (c) 2024 Ibad Desmukh. All rights reserved
 import math
 
 def count_digits(number):
-	"""Return the number of digits in the given number"""
-	if number == 0:
-		return 1
-	# log10 provides the power to raise 10 to in order to get the number, and floor rounds it to lowest integer
-	return math.floor(math.log10(abs(number))) + 1
+    """Return the number of digits in the given number"""
+    if number == 0:
+        return 1
+    # log10 provides the power to raise 10 to in order to get the number, and floor rounds it to lowest integer
+    return math.floor(math.log10(abs(number))) + 1
 
 def rec_int_mult(x, y):
-	"""
+    """
     Recursively multiply two positive integers using the RecIntMult algorithm
     
     Args:
@@ -28,37 +28,37 @@ def rec_int_mult(x, y):
     Returns:
     The product of x and y
     """
-	
-	# Base case: check if the input numbers are single-digit and if so return their product directly
-	if x < 10 and y < 10:
-		return x * y
-	
-	# Recursive case
-	# Calculate n, the number of digits in x and y
-	n_x, n_y = count_digits(x), count_digits(y)
+    
+    # Base case: check if the input numbers are single-digit and if so return their product directly
+    if x < 10 and y < 10:
+        return x * y
+    
+    # Recursive case
+    # Calculate n, the number of digits in x and y
+    n_x, n_y = count_digits(x), count_digits(y)
 
-	# Determine n, as maximum of n_x and n_y
-	n = max(n_x, n_y)
+    # Determine n, as maximum of n_x and n_y
+    n = max(n_x, n_y)
 
-	# Split x into a and b, y into c and d
-	a = x // (10**(n_x//2))
-	b = x % (10**(n_x//2))
-	c = y // (10**(n_y//2))
-	d = y % (10**(n_y//2))
+    # Split x into a and b, y into c and d
+    a = x // (10**(n_x//2))
+    b = x % (10**(n_x//2))
+    c = y // (10**(n_y//2))
+    d = y % (10**(n_y//2))
 
-	# Implement recursive multiplication
-	ac = rec_int_mult(a, c)
-	ad = rec_int_mult(a, d)
-	bc = rec_int_mult(b, c)
-	bd = rec_int_mult(b, d)
+    # Implement recursive multiplication
+    ac = rec_int_mult(a, c)
+    ad = rec_int_mult(a, d)
+    bc = rec_int_mult(b, c)
+    bd = rec_int_mult(b, d)
 
-	# Calculate each term of the formula
-	term_1 = 10**n * ac
-	term_2 = 10**(n // 2) * (ad + bc)
-	term_3 = bd
+    # Calculate each term of the formula
+    term_1 = 10**n * ac
+    term_2 = 10**(n // 2) * (ad + bc)
+    term_3 = bd
 
-	# Perform basic addition to compute x.y
-	return term_1 + term_2 + term_3
+    # Perform basic addition to compute x.y
+    return term_1 + term_2 + term_3
 
 # Test the function
 print(rec_int_mult(24, 36))
